b77a79d11f2aa8213d44654fb51db38436305f8c
DESKTOP-C7AUPQ0\adity <aditya.mk.sharma@gmail.com>
2017-06-18 14:16:27 -0500

    CFN practice

---
+++ create_CFN.py
@@ -0,0 +1,46 @@
+import boto3 as b
+import json
+from datetime import datetime
+import logging_module
+
+def main():
+    #cfn_client = b.client('cloudformation')
+    cfn_template = datetime.now().strftime("%Y%m%d-%H%M%S") + '_cfn_template.json'
+
+    data = {
+        "AWSTemplateFormatVersion" : "version date",
+
+        "Description" : "JSON string",
+
+        "Metadata" : {
+            "template metadata": ""
+        },
+
+        "Parameters" : {
+            "set of parameters": ""
+        },
+
+        "Mappings" : {
+            "set of mappings": ""
+        },
+
+        "Conditions" : {
+            "set of conditions": ""
+        },
+
+        "Transform" : {
+            "set of transforms": ""
+        },
+
+        "Resources" : {
+            "set of resources": ""
+        },
+
+        "Outputs" : {
+            "set of outputs": ""
+        }
+    }
+    with open(cfn_template, 'w') as outfile:
+        json.dump(data, outfile)
+
+if __name__ == '__main__':main()
