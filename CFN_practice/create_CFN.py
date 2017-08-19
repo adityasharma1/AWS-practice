c978698cbb33599adebed33f443e22b000c26b17
DESKTOP-C7AUPQ0\adity <aditya.mk.sharma@gmail.com>
2017-08-19 13:06:23 -0500

    8/19/2017 update

--- CFN_practice/create_CFN.py
+++ CFN_practice/create_CFN.py
@@ -1,15 +1,15 @@
 import json
 from datetime import datetime
-
+import boto3 as b
 
 def main():
-    #cfn_client = b.client('cloudformation')
+    cfn_client = b.client('cloudformation',region_name='us-west-2')
     cfn_template = datetime.now().strftime("%Y%m%d-%H%M%S") + '_cfn_template.json'
 
     data = {
-        "AWSTemplateFormatVersion" : "version date",
+        "AWSTemplateFormatVersion" : datetime.now().strftime("%Y%m%d-%H%M%S"),
 
-        "Description" : "JSON string",
+        "Description" : "test CFN",
 
         "Metadata" : {
             "template metadata": ""
